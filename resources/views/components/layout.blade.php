97cd82276a5185734b338757cb3a65bf40ef1592
gazali14 <gazali.1494@gmail.com>
2024-11-27 11:26:18 +0700

    add navbar temp

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="icon" type="image/png" href="{{ asset('img/logo2.png') }}">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <title>Posyandu Pikasti</title>
+</head>
+<body class="h-full">
+  <div class="min-h-full">
+    <x-navbar></x-navbar>
+    <main class="bg-white">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        {{ $slot }}
+      </div>
+    </main>
+  </div>
+  
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,65 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Routes for orang_tua before_login
 Route::get('/', function () {
-    return view('welcome');
+    return view('orang_tua.before_login.home');
+})->name('home');
+
+Route::prefix('orang_tua/before_login')->group(function () {
+    Route::get('/home', function () {
+        return view('orang_tua.before_login.home');
+    })->name('orang_tua.before_login.home');
+    Route::get('/dokumentasi', function () {
+        return view('orang_tua.before_login.dokumentasi');
+    })->name('orang_tua.before_login.dokumentasi');
+    Route::get('/jadwal', function () {
+        return view('orang_tua.before_login.jadwal');
+    })->name('orang_tua.before_login.jadwal');
+    Route::get('/profil_kader', function () {
+        return view('orang_tua.before_login.profil_kader');
+    })->name('orang_tua.before_login.profil_kader');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/orang_tua/dashboard', function () {
+    return view('orang_tua.dashboard');
+})->name('orang_tua.dashboard');
+
+
+// Routes for admin
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+    Route::get('/kelola_jadwal', function () {
+        return view('admin.kelola_jadwal');
+    })->name('admin.kelola_jadwal');
+
+    Route::get('/kelola_kader', function () {
+        return view('admin.kelola_kader');
+    })->name('admin.kelola_kader');
+
+    Route::get('/presensi_kader', function () {
+        return view('admin.presensi_kader');
+    })->name('admin.presensi_kader');
+});
+
+// Routes for kader
+Route::prefix('kader')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('kader.dashboard');
+    })->name('kader.dashboard');
+
+    Route::get('/konsultasi', function () {
+        return view('kader.konsultasi');
+    })->name('kader.konsultasi');
+
+    Route::get('/perkembangan_bayi', function () {
+        return view('kader.perkembangan_bayi');
+    })->name('kader.perkembangan_bayi');
+
+    Route::get('/presensi_bayi', function () {
+        return view('kader.presensi_bayi');
+    })->name('kader.presensi_bayi');
 });
