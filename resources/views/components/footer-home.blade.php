a24b9ff0a9144194774b30b54f70699c56326c64
Raditya Hizra Maharani <222212824@stis.ac.id>
2024-12-06 01:42:52 +0700

    add footer

---
+++ resources/views/components/footer-home.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-[#62bcb1] text-white items-center justify-center py-10">
+    <p class="text-center text-sm md:text-base lg:text-lg">Copyright © 2024/2025 by Posyandu Pikasti. All Rights Reserved.</p>
+    <p class="text-center text-sm md:text-base lg:text-lg">RT 013-014 RW 07 Kelurahan Pondok Bambu, Kecamatan Duren Sawit | Telp: (021) **** ****</p>
+  </div>
--- resources/views/orang_tua/before_login/dokumentasi.blade.php
+++ resources/views/orang_tua/before_login/dokumentasi.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
   <h3>Nanti ngoding disini</h3>
 </x-layout>
+
+{{-- FOOTER --}}
+<x-footer-home></x-footer-home>
