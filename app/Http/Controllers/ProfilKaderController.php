d805b8e941b5a3fb7b22c29cdabf1baa78d36150
Alfitra Rifa Geandra Siregar <222212476@stis.ac.id>
2024-12-09 21:44:28 +0700

    halaman profil kader

---
+++ app/Http/Controllers/ProfilKaderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Kader;
+
+class ProfilKaderController extends Controller
+{
+    public function index()
+    {
+        // Ambil semua data kader
+        $kaders = Kader::select('nama', 'jabatan', 'foto')->get();
+
+        // Kirim data kader ke view
+        return view('orang_tua.before_login.profil_kader', compact('kaders'));
+    }
+}
