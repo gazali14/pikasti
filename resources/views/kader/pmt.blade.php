c30903535b4ca6fb140184a2696f0ffc369fa95b
Reamare45 <europanjaitan@gmail.com>
2024-12-31 01:42:33 +0700

    Perbaikan PMT

--- resources/views/kader/pmt.blade.php
+++ resources/views/kader/pmt.blade.php
@@ -2,16 +2,16 @@
     <div class="p-2 min-h-screen max-h-96">
         <!-- Judul Halaman -->
         <div class="text-center my-4">
-            <h1 class="text-2xl font-bold text-[#353535] font-poppins">Pemberian Vitamin</h1>
+            <h1 class="text-2xl font-bold text-[#353535] font-poppins">Pemberian Makanan Tambahan</h1>
         </div>
 
 
-        <!-- Tabel Daftar Vitamin dengan Scroll -->
+        <!-- Tabel Daftar PMT dengan Scroll -->
         <div class="p-2 bg-[rgba(191,243,221,0.8)] rounded shadow">
-            <x-tabel-vitamin />
+            <x-tabel-pmt />
         </div>
 
-        <!-- Form Tambah Vitamin -->
-        <x-form-tambah-vitamin></x-form-tambah-vitamin>
+        <!-- Form Tambah PMT -->
+        <x-form-tambah-pmt></x-form-tambah-pmt>
     </div>
 </x-layout-kader>
