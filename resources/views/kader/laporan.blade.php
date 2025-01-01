dea175d8d317f12536a706f31e9d0c1689a8aab4
Raditya Hizra Maharani <222212824@stis.ac.id>
2025-01-01 20:39:21 +0700

    revisi laporan

--- resources/views/kader/laporan.blade.php
+++ resources/views/kader/laporan.blade.php
@@ -1,73 +1,213 @@
 <x-layout-kader>
-  <p class="font-bold text-2xl text-[#353535] text-center mb-10">Generate Laporan</p>
-
-  <div class="flex flex-col items-start px-8 max-w-xl mx-auto">
-    <!-- Container for inputs -->
-    <div class="space-y-6 w-full">
-      <!-- Jenis Variabel -->
-      <div>
-        <div class="font-bold text-xl text-left mb-4">Jenis Variabel:</div>
-        <div class="relative w-full">
-          <!-- Dropdown Toggle -->
-          <button id="dropdownToggle" class="flex items-center justify-between text-sm w-full bg-white px-4 py-2 rounded-lg shadow focus:outline-none focus:ring focus:ring-[#41a99d]">
-            Pilih Jenis Variabel
-            <img src="{{ asset('img/Dropdown.png') }}" alt="" class="w-3 h-auto">
-          </button>
-          <!-- Dropdown Menu -->
-          <div id="dropdownMenu" class="absolute z-10 hidden w-full mt-2 bg-white border border-gray-200 rounded-lg shadow-lg">
-            <!-- Search Box -->
-            <div class="p-2">
-              <input
-                type="text"
-                id="dropdownSearch"
-                class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-[#41a99d]"
-                placeholder="Search..."
-              />
-            </div>
-            <!-- Dropdown Items -->
-            <ul id="dropdownItems" class="max-h-48 overflow-y-auto">
-              <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer">Semua Variabel</li>
-              <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer">Presensi Bayi</li>
-              <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer">Kondisi Bayi</li>
-              <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer">Vitamin</li>
-              <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer">PMT</li>
-              <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer">Konsultasi</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <!-- Tanggal -->
-      <div>
-        <div class="font-bold text-xl text-left mb-4">Tanggal:</div>
+  <p class="font-bold text-2xl text-[#353535] text-center mb-10">Laporan Summary</p>
+    <!-- Tanggal -->
+     <div>
+      <div class="font-bold text-xl text-left mb-4">Pilih Tanggal:</div>
         <input type="date" id="search-date" class="w-full p-2 border border-[#62BCB1] rounded-lg">
-      </div>
-
-      <!-- Format -->
-      <div>
-        <label for="format" class="font-bold text-xl text-left mb-2 block">Format:</label>
-        <div class="flex">
-          <button id="pdfButton" class="pdf w-32 py-2 text-center bg-white text-black border border-gray-300 rounded-l-lg hover:bg-gray-100 focus:outline-none focus:ring focus:ring-[#41a99d]" onclick="selectFormat('PDF')">
-            PDF
-          </button>
-          <button id="excelButton" class="excel w-32 py-2 text-center bg-white text-black border border-gray-300 rounded-r-lg hover:bg-gray-100 focus:outline-none focus:ring focus:ring-[#41a99d]" onclick="selectFormat('EXCEL')">
-            EXCEL
-          </button>
-        </div>
-      </div>
     </div>
 
-    <!-- Button Generate -->
-    <div class="mt-8 w-full flex justify-end">
-      <button id="generate-button" 
-          type="submit"
-          style="background-color: #4EC3AF; color: #ffffff; padding: 10px 20px;
-          border-radius: 8px; font-size: 16px; cursor: pointer; font-weight: bold;
-          box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); 
-          transition: background-color 0.3s ease, transform 0.1s ease;">
-          Generate
-      </button>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-0 bg-[#a1c47b] w-full border shadow-md rounded-lg p-4">
+      {{-- Summary --}}
+      <div class="space-y-4">
+        <div class="space-y-2">
+          <div class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Jumlah Kader yang Hadir </div>
+              <div class="w-1/2">: </div>
+          </div>
+
+            <p class="font-medium">Jumlah Vitamin A bagi Balita</p>
+              <ul class="pl-1">
+                <li class="flex flex-wrap items-center">
+                  <div class="w-1/2 font-medium">Vitamin A Merah</div>
+                  <div class="w-1/2 text-left">: </div>
+              </li>
+              <li class="flex flex-wrap items-center">
+                <div class="w-1/2 font-medium">Vitamin A Merah (12-59 Bulan)</div>
+                <div class="w-1/2 text-left">: </div>
+            </li>
+              <li class="flex flex-wrap items-center">
+                <div class="w-1/2 font-medium">Vitamin A Biru</div>
+                <div class="w-1/2 text-left">: </div>
+            </li>
+            <li class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Vitamin A Biru (6-11 Bulan)</div>
+              <div class="w-1/2 text-left">: </div>
+          </li>
+              </ul>
+
+          <div class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Jumlah Seluruh Balita (S)</div>
+              <div class="w-1/2">: 0 P 13 L </div>
+          </div>
+          <div class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Jumlah Balita Memiliki KMS (K)</div>
+              <div class="w-1/2">: 0 P 0 L </div>
+          </div>
+          <p class="font-bold">GIZI</p>
+          <div class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Jumlah Balita Ditimbang (D)</div>
+              <div class="w-1/2">: 1 P 1 L</div>
+            </div>
+              <ul class="pl-1">
+                <li class="flex flex-wrap items-center">
+                  <div class="w-1/2 font-medium">0-23 Bulan yg Ditimbang</div>
+                  <div class="w-1/2 text-left">: </div>
+              </li>
+              <li class="flex flex-wrap items-center">
+                <div class="w-1/2 font-medium">24-59 Bulan yg Ditimbang</div>
+                <div class="w-1/2 text-left">: </div>
+            </li>
+              </ul>
+          
+          <div class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Jumlah Balita Naik BB (N)</div>
+              <div class="w-1/2">: 12 P 13 L</div>
+          </div>
+
+          <div class="flex flex-wrap items-center">
+            <div class="w-1/2 font-medium">Jumlah Balita Tetap BB (T)</div>
+            <div class="w-1/2">: 12 P 13 L</div>
+          </div>
+          <div class="flex flex-wrap items-center">
+            <div class="w-1/2 font-medium">Jumlah Balita Pertama Kali Ditimbang (B)</div>
+            <div class="w-1/2">: 12 P 13 L</div>
+          </div>
+
+          <div class="flex flex-wrap items-center">
+            <div class="w-1/2 font-medium">Jumlah Balita Stunting (BGM)</div>
+            <div class="w-1/2">: 12 P 13 L</div>
+          </div>
+
+              <p class="font-bold">JUMLAH BAYI</p>
+              <div class="flex flex-wrap items-center">
+                <div class="w-1/2 font-medium">Jumlah Bayi 0-5 Bulan</div>
+                <div class="w-1/2">: 16 </div>
+            </div>
+              <div class="flex flex-wrap items-center">
+                <div class="w-1/2 font-medium">Jumlah Bayi 6-11 Bulan</div>
+                <div class="w-1/2">: 16 </div>
+            </div>
+              <div class="flex flex-wrap items-center">
+                <div class="w-1/2 font-medium">Jumlah Bayi 12-23 Bulan</div>
+                <div class="w-1/2">: 16 </div>
+            </div>
+              <div class="flex flex-wrap items-center">
+                <div class="w-1/2 font-medium">Jumlah Bayi 24-59 Bulan</div>
+                <div class="w-1/2">: 16 </div>
+            </div>
+            <div class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Jumlah Bayi 0-23 Bulan</div>
+              <div class="w-1/2">: 16 </div>
+          </div>
+            <div class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Jumlah Bayi < 1 Tahun Ditimbang</div>
+              <div class="w-1/2">: 16 </div>
+          </div>
+            <div class="flex flex-wrap items-center">
+              <div class="w-1/2 font-medium">Jumlah Balita 1-4 Tahun Ditimbang</div>
+              <div class="w-1/2">: 16 </div>
+          </div>      
+      </div>
     </div>
+    <div class="mt-2 space-y-4">
+      <div class="space-y-2">
+    <p class="font-bold">PMT</p>
+      <div class="flex flex-wrap items-center">
+        <div class="w-1/2 font-medium">Ada/Tidak</div>
+        <div class="w-1/2">: </div>
+    </div>
+      <div class="flex flex-wrap items-center">
+        <div class="w-1/2 font-medium">Keterangan</div>
+        <div class="w-1/2">: </div>
+    </div>
+
+    <p class="font-bold">IMUNISASI</p>
+      <div class="flex flex-wrap items-center">
+        <div class="w-1/2 font-medium">Jumlah yang Imunisasi BCG</div>
+        <div class="w-1/2">: 3 P 10 L </div>
+    </div>
+      <div class="flex flex-wrap items-center">
+        <div class="w-1/2 font-medium">Jumlah yang Imunisasi Polio I</div>
+        <div class="w-1/2">: 3 P 10 L </div>
+    </div>
+      <div class="flex flex-wrap items-center">
+        <div class="w-1/2 font-medium">Jumlah yang Imunisasi Polio II</div>
+        <div class="w-1/2">: 3 P 10 L </div>
+    </div>
+    <div class="flex flex-wrap items-center">
+      <div class="w-1/2 font-medium">Jumlah yang Imunisasi Polio III</div>
+      <div class="w-1/2">: 3 P 10 L </div>
+    </div>
+    <div class="flex flex-wrap items-center">
+      <div class="w-1/2 font-medium">Jumlah yang Imunisasi Polio IV</div>
+      <div class="w-1/2">: 3 P 10 L </div>
+    </div>
+      <div class="flex flex-wrap items-center">
+        <div class="w-1/2 font-medium">Jumlah yang Imunisasi Campak</div>
+        <div class="w-1/2">: 3 P 10 L </div>
+    </div>
+      <div class="flex flex-wrap items-center">
+        <div class="w-1/2 font-medium">Jumlah yang Imunisasi Hepatitis B 1</div>
+        <div class="w-1/2">: 3 P 10 L </div>
+    </div>
+    <div class="flex flex-wrap items-center">
+      <div class="w-1/2 font-medium">Jumlah yang Imunisasi Hepatitis B 2</div>
+      <div class="w-1/2">: 3 P 10 L </div>
+  </div>
+    <div class="flex flex-wrap items-center">
+      <div class="w-1/2 font-medium">Jumlah yang Imunisasi Hepatitis B 3</div>
+      <div class="w-1/2">: 3 P 10 L </div>
+  </div>
+
+    <p class="font-bold">JUMLAH PENGUNJUNG</p>
+    <p class="font-medium">Bayi</p>
+              <ul class="pl-1">
+                <li class="flex flex-wrap items-center">
+                  <div class="w-1/2 font-medium">0-12 Bulan yang Baru</div>
+                  <div class="w-1/2 text-left">: 1 P 1 L </div>
+              </li>
+              <li class="flex flex-wrap items-center">
+                <div class="w-1/2 font-medium">0-12 Bulan yang Lama</div>
+                <div class="w-1/2 text-left">: 3 P 5 L </div>
+            </li>
+        </ul>
+        <p class="font-medium">Petugas yang Hadir</p>
+        <ul class="pl-1">
+          <li class="flex flex-wrap items-center">
+            <div class="w-1/2 font-medium">Kader</div>
+            <div class="w-1/2 text-left">: </div>
+        </li>
+          <li class="flex flex-wrap items-center">
+            <div class="w-1/2 font-medium">PLKB</div>
+            <div class="w-1/2 text-left">: </div>
+        </li>
+        <li class="flex flex-wrap items-center">
+          <div class="w-1/2 font-medium">Medis</div>
+          <div class="w-1/2 text-left">: </div>
+      </li>
+      <li class="flex flex-wrap items-center">
+        <div class="w-1/2 font-medium">Para Medis</div>
+        <div class="w-1/2 text-left">: </div>
+    </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <p class="p-2 text-sm">Keterangan:</p>
+    <div class="pl-3 text-sm">P: Perempuan</div>
+    <div class="pl-3 text-sm">L: Laki-Laki</div>
+
+  <div class="mt-3 font-bold pl-2 flex justify-end">Generate Data Mentah ke Excel</div>
+  <div class="mt-2 w-full flex justify-end">
+    <button id="generate-button" 
+        type="submit"
+        style="background-color: #4EC3AF; color: #ffffff; padding: 10px 20px;
+        border-radius: 8px; font-size: 16px; cursor: pointer; font-weight: bold;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); 
+        transition: background-color 0.3s ease, transform 0.1s ease;">
+        Generate
+    </button>
+    <!-- Kalau bisa generatenya dengan data presensi kadernya ya-->
   </div>
 
   <script>
@@ -85,58 +225,4 @@
       });
   </script>
 
-  <script>
-      // Fungsi untuk memilih format
-      function selectFormat(format) {
-          document.getElementById('pdfButton').style.backgroundColor = '#fff';
-          document.getElementById('pdfButton').style.color = '#000';
-          document.getElementById('pdfButton').style.border = '1px solid #ddd';
-
-          document.getElementById('excelButton').style.backgroundColor = '#fff';
-          document.getElementById('excelButton').style.color = '#000';
-          document.getElementById('excelButton').style.border = '1px solid #ddd';
-
-          if (format === 'PDF') {
-              document.getElementById('pdfButton').style.backgroundColor = '#4EC3AF';
-              document.getElementById('pdfButton').style.color = '#fff';
-          } else if (format === 'EXCEL') {
-              document.getElementById('excelButton').style.backgroundColor = '#4EC3AF';
-              document.getElementById('excelButton').style.color = '#fff';
-          }
-      }
-
-      const toggleButton = document.getElementById('dropdownToggle');
-      const dropdownMenu = document.getElementById('dropdownMenu');
-      const searchInput = document.getElementById('dropdownSearch');
-      const dropdownItems = document.getElementById('dropdownItems');
-      const items = dropdownItems.querySelectorAll('li');
-
-      toggleButton.addEventListener('click', () => {
-        dropdownMenu.classList.toggle('hidden');
-      });
-
-      document.addEventListener('click', (e) => {
-        if (!dropdownMenu.contains(e.target) && !toggleButton.contains(e.target)) {
-          dropdownMenu.classList.add('hidden');
-        }
-      });
-
-      searchInput.addEventListener('input', (e) => {
-        const searchValue = e.target.value.toLowerCase();
-        items.forEach((item) => {
-          if (item.textContent.toLowerCase().includes(searchValue)) {
-            item.classList.remove('hidden');
-          } else {
-            item.classList.add('hidden');
-          }
-        });
-      });
-
-      items.forEach((item) => {
-        item.addEventListener('click', () => {
-          toggleButton.textContent = item.textContent;
-          dropdownMenu.classList.add('hidden');
-        });
-      });
-  </script>
 </x-layout-kader>
