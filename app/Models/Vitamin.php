0c7183879df64a6fb593aa8a1b7890bf20b78011
Reamare45 <europanjaitan@gmail.com>
2025-01-05 01:14:26 +0700

    Perbaikan Model Vitamin

--- app/Models/Vitamin.php
+++ app/Models/Vitamin.php
@@ -12,19 +12,9 @@ class Vitamin extends Model
 
     protected $table = 'vitamins';
 
-    protected $fillable = ['nama_bayi','vitamin','id_bayi', 'id_kegiatan'];
-
-    /**
-     * Relasi ke model Bayi
-     */
-    public function bayis()
-    {
-        return $this->belongsTo(Bayi::class, 'id_bayi'); 
-    }
-
-    public function jadwal()
-    {
-        return $this->belongsTo(Jadwal::class, 'id_kegiatan');
-    }
-
+    protected $fillable = [
+        'nik_bayi',
+        'tanggal',
+        'vitamin',
+    ];
 }
