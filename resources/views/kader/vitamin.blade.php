07b33dcd3ddccdfc19016d04d6b878cfb1d8eb64
Reamare45 <europanjaitan@gmail.com>
2025-01-05 01:17:01 +0700

    Perbaikan view vitamin

--- resources/views/kader/vitamin.blade.php
+++ resources/views/kader/vitamin.blade.php
@@ -4,8 +4,8 @@
             <h1 class="text-2xl font-bold text-[#353535] font-poppins">Pemberian Vitamin</h1>
         </div>
         <div class="p-2 bg-[rgba(191,243,221,0.8)] rounded shadow">
-            <x-tabel-vitamin :bayis="$bayis" />
+            <x-tabel-vitamin :bayiList="$bayiList" :vitaminData="$vitaminData" :selectedBayiNik="$selectedBayiNik" />
+
         </div>
-            <x-form-tambah-vitamin :jadwals="$jadwals"></x-form-tambah-vitamin>
     </div>
 </x-layout-kader>
