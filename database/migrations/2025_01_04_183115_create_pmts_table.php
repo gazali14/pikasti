8217cca42877903ea5c80ef678a27c0eff1ebff9
Reamare45 <europanjaitan@gmail.com>
2025-01-05 02:12:23 +0700

    Penambahan migrasi untuk PMT

---
+++ database/migrations/2025_01_04_183115_create_pmts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pmts', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik_bayi');
+            $table->date('tanggal'); // Kolom tanggal
+            $table->string('pmt',100);
+            $table->timestamps();
+
+             // Relasi ke tabel bayi
+             $table->foreign('nik_bayi')->references('nik')->on('bayis')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pmts');
+    }
+};
