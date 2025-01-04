610ae5da606d54d46fa646c9677c1af6f4763f78
Reamare45 <europanjaitan@gmail.com>
2025-01-05 02:10:17 +0700

    Perbaikan view pmt

--- resources/views/kader/pmt.blade.php
+++ resources/views/kader/pmt.blade.php
@@ -2,16 +2,14 @@
     <div class="p-2 min-h-screen max-h-96">
         <!-- Judul Halaman -->
         <div class="text-center my-4">
-            <h1 class="text-2xl font-bold text-[#353535] font-poppins">Pemberian Makanan Tambahan</h1>
+            <h1 class="text-2xl font-bold text-[#353535] font-poppins">Pemberian Makanan Tambahan (PMT)</h1>
         </div>
 
 
         <!-- Tabel Daftar PMT dengan Scroll -->
         <div class="p-2 bg-[rgba(191,243,221,0.8)] rounded shadow">
-            <x-tabel-pmt />
+            <x-tabel-pmt :bayiList="$bayiList" :pmtData="$pmtData" :selectedBayiNik="$selectedBayiNik" />          
         </div>
 
-        <!-- Form Tambah PMT -->
-        <x-form-tambah-pmt></x-form-tambah-pmt>
     </div>
 </x-layout-kader>
